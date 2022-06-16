7afbe919849217bd1b4155529108aba83c8d14f1
Dodi Triwibowo <silveriusdodi@gmail.com>
2022-06-17 00:55:15 +0700

    Add ReplyAs and setter for StatusCode

--- response.go
+++ response.go
@@ -6,12 +6,13 @@ import (
 )
 
 type Response struct {
-	w http.ResponseWriter
-	Status int `json:"status"`
-	StatusNumber string `json:"status_number"`
-	StatusCode string `json:"status_code"`
-	StatusMessage string `json:"status_message"`
-	Data interface{} `json:"data"`
+	w              http.ResponseWriter
+	HttpStatusCode int         `json:"-"`
+	Status         int         `json:"status"`
+	StatusNumber   string      `json:"status_number"`
+	StatusCode     string      `json:"status_code"`
+	StatusMessage  string      `json:"status_message"`
+	Data           interface{} `json:"data"`
 }
 
 func PlugResponse(w http.ResponseWriter) *Response {
@@ -31,8 +32,15 @@ func (r *Response) SetHttpCode(code int) *Response {
 	return r
 }
 
+func (r *Response) ReplyAs(res Response) error {
+	if res.HttpStatusCode != 0 {
+		r.w.WriteHeader(res.HttpStatusCode)
+	}
+	return r.Reply(res.Status, res.StatusNumber, res.StatusCode, res.StatusMessage, res.Data)
+}
+
 // Reply 'data' arguments only used on index 0 */
-func (r *Response) Reply(status int, number string, code string, message string, data... interface{}) error {
+func (r *Response) Reply(status int, number string, code string, message string, data ...interface{}) error {
 	r.w.Header().Set("Content-Type", "application/json")
 
 	r.Status = status
@@ -47,11 +55,11 @@ func (r *Response) Reply(status int, number string, code string, message string,
 }
 
 // ReplyFailed 'data' arguments only used on index 0 */
-func (r *Response) ReplyFailed(number string, code string, message string, data... interface{}) error {
+func (r *Response) ReplyFailed(number string, code string, message string, data ...interface{}) error {
 	return r.Reply(0, number, code, message, data...)
 }
 
 // ReplySuccess 'data' arguments only used on index 0 */
-func (r *Response) ReplySuccess(number string, code string, message string, data... interface{}) error {
+func (r *Response) ReplySuccess(number string, code string, message string, data ...interface{}) error {
 	return r.Reply(1, number, code, message, data...)
 }
